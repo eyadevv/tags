2c9896f9f0c8bfc6cdad3f3322fd13ec6fe44902
eyadevv <eyadevv@gmail.com>
2023-08-15 18:07:04 +0200

    reset

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,18 +17,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: {
-          50: "#eff6ff",
-          100: "#dbeafe",
-          200: "#bfdbfe",
-          300: "#93c5fd",
-          400: "#60a5fa",
-          500: "#3b82f6",
-          600: "#2563eb",
-          700: "#1d4ed8",
-          800: "#1e40af",
-          900: "#1e3a8a",
-        },
+        primaryDark:"black",
+        primaryLight:"white",
+        secondaryDark:"gray",
+        secondaryLight:"green",
+        BRAND:"purple"
       },
       fontFamily: {
         body: [
