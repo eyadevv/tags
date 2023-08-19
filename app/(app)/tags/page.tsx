af89bef8bb3bdbdef9ba30d9bbb889042e1bbcb9
eyadevv <eyadevv@gmail.com>
2023-08-19 17:53:09 +0200

    Tag simple example

--- app/(app)/tags/page.tsx
+++ app/(app)/tags/page.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link"
+import Tag from "@/components/Tag/Tag"
 const page = async () => {
 
   return (
     <div className="w-full h-full flex justify-center items-start " >
+      <Tag />
 
     </div>
   )
---
+++ components/Tag/Tag.tsx
@@ -0,0 +1,12 @@
+const Tag = () => {
+  return (
+    <div className="w-72 h-72 bg-primary gap-2 rounded flex flex-col justify-center items-center" >
+        <div className="bg-white w-20 h-20 rounded-full" ></div>
+        <p className="font-bold text-lg" >224776</p>
+        <p className="font-bold text-lg" >Eyad Faisal Nimir Hassan</p>
+        <p className="font-bold text-lg" >0967098861</p>
+        {/* <p className=" text-opacity-0" >tags.sd</p> */}
+    </div>
+  )
+}
+export default Tag
