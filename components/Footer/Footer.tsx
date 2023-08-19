3aa77846536fa57789efaf48791dd3ebceea5e80
eyadevv <eyadevv@gmail.com>
2023-08-19 17:36:16 +0200

    footer

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <footer className="w-11/12 h-4 p-4 flex justify-center items-center" >
+        <p>Developed By Eyad Faisal</p>
+    </footer>
+  )
+}
+export default Footer
