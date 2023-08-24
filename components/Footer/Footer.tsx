ff9a4e98b456e308dfab0f5822fc535ca0f1b4b2
eyadevv <eyadevv@gmail.com>
2023-08-24 06:25:19 +0200

    edited Tag.tsx Footer.tsx

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="w-11/12 h-4 p-4 flex justify-center items-center" >
-        <p>Developed By Eyad Faisal</p>
+        <a href="https://github.com/eyadevv" about="_blank" target="_blank">Developed By Eyad Faisal</a>
     </footer>
   )
 }
--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -2,17 +2,17 @@ import { Button } from "../Button/Button"
 const Tag = ({bank , account , name , number ,type }:any) => {
   return (
     <div className="w-72 h-72 gap-2 py-2 flex justify-center items-center rounded-xl flex-col" >
-        <div className="w-5/6 relative h-5/6 bg-primary gap-2 rounded flex flex-col justify-center items-center" >
+        <span className="w-5/6 relative h-5/6 bg-primary gap-2 rounded flex flex-col justify-center items-center" >
       <p className="absolute top-2 left-2" >{type}</p>
 
         <p className="bg-white w-20 h-20 rounded-full" ></p>
         <p className="font-bold text-lg" >{account}</p>
         <p className="font-bold text-lg" >{name}</p>
         <p className="font-bold text-lg" >{number}</p>
-    </div>
-    <div className="w-5/6 flex justify-around items-center" >
+    </span>
+    <span className="w-5/6 flex justify-around items-center" >
         <Button href="/studio" intent='primary'>أستخدم</Button>
-    </div>
+    </span>
     </div>
 
   )
