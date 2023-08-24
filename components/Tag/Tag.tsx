979a0032475207889ecd1b5a249c0ca0c4a6ad56
eyadevv <eyadevv@gmail.com>
2023-08-24 06:16:00 +0200

    edited Tag.tsx

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -1,19 +1,17 @@
 import { Button } from "../Button/Button"
-const Tag = () => {
+const Tag = ({bank , account , name , number ,type }:any) => {
   return (
     <div className="w-72 h-72 gap-2 py-2 flex justify-center items-center rounded-xl flex-col" >
-        <div className="w-5/6 h-5/6 bg-primary gap-2 rounded flex flex-col justify-center items-center" >
+        <div className="w-5/6 relative h-5/6 bg-primary gap-2 rounded flex flex-col justify-center items-center" >
+      <p className="absolute top-2 left-2" >{type}</p>
+
         <p className="bg-white w-20 h-20 rounded-full" ></p>
-        <p className="font-bold text-lg" >224776</p>
-        <p className="font-bold text-lg" >Eyad Faisal Nimir Hassan</p>
-        <p className="font-bold text-lg" >0967098861</p>
-        {/* <p className=" text-opacity-0" >tags.sd</p> */}
+        <p className="font-bold text-lg" >{account}</p>
+        <p className="font-bold text-lg" >{name}</p>
+        <p className="font-bold text-lg" >{number}</p>
     </div>
     <div className="w-5/6 flex justify-around items-center" >
-        <Button href="/tag" intent='primary'>Use</Button>
-        <Button href="/studio" intent='secondary' >Edit</Button>
-
-        
+        <Button href="/studio" intent='primary'>أستخدم</Button>
     </div>
     </div>
 
