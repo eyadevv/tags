f05d8484701ec7b0a4e410c54cb20444d7f6237e
eyadevv <eyadevv@gmail.com>
2023-08-29 11:06:43 +0200

    database query

--- app/(app)/tags/[type]/page.tsx
+++ app/(app)/tags/[type]/page.tsx
@@ -1,10 +1,24 @@
 import Tag from "@/components/Tag/Tag"
+import PRISMA from "@/lib/prisma"
+import { TAGSTYPE } from "@prisma/client"
+const page = async ({ params }: { params: { type: string } }) => {
+  const type = params.type.toUpperCase()
+  const tags = await PRISMA.tag
+    .findMany({
+      where: {
+        type: type as TAGSTYPE,
+      },
+    })
+    .then((res) => {
+      return res?.length > 0 ? res : null
+    })
+    .catch((err) => null)
 
-const page = ({ params }: any) => {
   return (
     <div className="flex min-h-screen w-full items-center justify-center bg-blue-900">
-      {params.type}
-      <Tag bank="bankak" account="123435" name="eyad faisal nimir" number="0967098861" />
+      {tags
+        ? tags.map((tag) => <Tag bank="bankak" account="123435" name="eyad faisal nimir" number="0967098861" />)
+        : "No Tags Found"}
     </div>
   )
 }
