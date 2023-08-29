641b66f190af60bd413b7149f40c8201f29dd21e
eyadevv <eyadevv@gmail.com>
2023-08-29 22:17:37 +0200

    fix type issues

--- src/app/(app)/tags/[type]/page.tsx
+++ src/app/(app)/tags/[type]/page.tsx
@@ -1,21 +1,24 @@
 import Tag from "@/src/components/Tag/Tag";
 import PRISMA from "@/lib/prisma";
+import { TAGSTYPE } from "@prisma/client";
 const page = async ({ params }: { params: { type: string } }) => {
   const type = params.type.toUpperCase();
   const tags = await PRISMA.tag
     .findMany({
       where: {
-        type,
+        type: type as TAGSTYPE,
       },
     })
-    .then((res: string | any[]) => {
+    .then((res: any) => {
       return res?.length > 0 ? res : null;
     })
     .catch(() => null);
 
   return (
     <div className="flex min-h-screen w-full items-center justify-center bg-blue-900">
-      {tags ? tags.map((tag: any) => <Tag data={tag} />) : "No Tags Found"}
+      {tags
+        ? tags?.map((tag: any, id: any) => <Tag key={id} data={tag} />)
+        : "No Tags Found"}
     </div>
   );
 };
