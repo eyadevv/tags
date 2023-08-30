20cbcd9c5fcfa9ae25227201dea8b88ccea4bdfb
eyadevv <eyadevv@gmail.com>
2023-08-30 11:35:38 +0200

    fix navigation

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav className="w-11/12 sticky bg-primary top-0 h-[15vh] flex flex-row-reverse justify-between items-center">
-      <Link href="/tags">
+    <nav className="w-full px-[6%] sticky top-0 h-[15vh] flex flex-row-reverse justify-between items-center bg-black">
+      <Link href="/tags/free">
         <h2 className="text-5xl font-bold">تاغز</h2>
       </Link>
-      <ul className="flex flex-row-reverse gap-2 w-1/3 justify-between items-center sm:hidden">
-        <Link href="/tags">الرئيسية</Link>
+      <ul className="flex flex-row-reverse gap-4 w-1/3 min-w-max justify-between items-center xs:hidden">
+        <Link href="/tags/free">الرئيسية</Link>
         <Link href="/studio">الأستديو</Link>
         <Link href="/library">بطاقاتي</Link>
         <Link
