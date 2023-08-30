9de93978fc026ccab9f9ce44052fa845238cb4dd
eyadevv <eyadevv@gmail.com>
2023-08-30 12:20:32 +0200

    seed setup

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,1 +1,47 @@
-import PRISMA from "@/lib/prisma";
+import { Prisma, PrismaClient } from "@prisma/client";
+
+const PRISMA = new PrismaClient();
+
+async function main() {
+  await PRISMA.$connect();
+
+  try {
+    await PRISMA.tag
+      .createMany({
+        skipDuplicates: true,
+        data: [
+          {
+            accountNumber: "123123",
+            bankName: "bankak",
+            name: "Eyad Faisal",
+            phoneNumber: "0967098861",
+            type: "FREE",
+            creatorName: "eyad faisal",
+            style: {
+              bgColor: "white",
+              bankRounded: "full",
+              textColor: "red",
+            },
+          },
+          {
+            accountNumber: "4556454",
+            bankName: "fawry",
+            name: "Eyad Faisal",
+            phoneNumber: "0967098861",
+            type: "FREE",
+            creatorName: "eyad faisal",
+            style: {
+              bgColor: "black",
+              bankRounded: "full",
+              textColor: "red",
+            },
+          },
+        ],
+      })
+      .then((res) => console.log(res));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
