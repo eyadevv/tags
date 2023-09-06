2438a29b7493c9be33d248e9eaa54f06cb269407
eyadevv <eyadevv@gmail.com>
2023-09-06 21:39:51 +0200

    moving from html2canvas to other solutions

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -1,24 +1,39 @@
 "use client";
 import FetchTag from "@/src/app/(app)/studio/FetchTag";
+import { useRef } from "react";
 import { BiSolidCrown, BiDollar } from "react-icons/bi";
-import { useEffect, useLayoutEffect, useState, useTransition } from "react";
-const Tag = () => {
-  let [isPending, startTransition] = useTransition();
-  const [data, setdata] = useState("");
+import html2canvas from "html2canvas";
+// import { useEffect, useLayoutEffect, useState, useTransition } from "react";
+const Tag = ({ data }) => {
+  // let [isPending, startTransition] = useTransition();
+  // const [data, setdata] = useState("");
 
-  useLayoutEffect(() => {
-    startTransition(async () => {
-      const data = await FetchTag();
-      setdata(data);
-    });
-  }, []);
+  // useLayoutEffect(() => {
+  //   startTransition(async () => {
+  //     const data = await FetchTag();
+  //     setdata(data);
+  //   });
+  // }, []);
   const { id, type, bank, account, name, phone, style }: any = data;
   // const { bg, bank: bankstyle, font, text, icons, barcode } = style;
+  const tagRef = useRef(null);
+
+  const download = (current) => {
+    console.log(tagRef.current);
+
+    html2canvas(current).then((canvas) => {
+      const link = document.createElement("a");
+      link.download = `${id}.svg`;
+      link.href = canvas.toDataURL();
+      link.click();
+    });
+  };
 
   return (
     <div className="flex  flex-shrink-0 h-72 w-72 flex-col items-center justify-center gap-2 rounded-xl py-2">
       <span
-        className={`relatve bg-gradient-to-bl to-accent from-primary flex h-5/6 w-5/6 flex-col items-center justify-center gap-2 rounded-xl`}
+        className={`relatve bg-gradient-to-bl to-accent from-primary flex h-5/6 w-5/6 flex-col items-center justify-center gap-2 rounded overflow-clip`}
+        ref={tagRef}
       >
         <p className="absolute left-2 top-2">
           {type === "PREMIUM" ? (
@@ -40,6 +55,12 @@ const Tag = () => {
         <p className="text-base font-bold">{name}</p>
         <p className="text-sm">{phone}</p>
       </span>
+      <button
+        className="p-4 bg-black "
+        onClick={() => download(tagRef.current)}
+      >
+        Download
+      </button>
     </div>
   );
 };
