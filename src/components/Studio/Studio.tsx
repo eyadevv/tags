4077ed8fd6c03f158e70d659cfafc7c62ebb817f
eyadevv <eyadevv@gmail.com>
2023-09-11 11:27:18 +0200

    custom types

--- src/components/Studio/Studio.tsx
+++ src/components/Studio/Studio.tsx
@@ -4,27 +4,6 @@ import Select from "./Select";
 import Bgpicker from "./Bgpicker";
 import { useEffect, useState } from "react";
 import ReactGPicker from "react-gcolor-picker";
-import * as React from "react";
-import * as SliderPrimitive from "@radix-ui/react-slider";
-import Radius from "./Radius";
-const Slider = React.forwardRef<
-  React.ElementRef<typeof SliderPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <SliderPrimitive.Root
-    ref={ref}
-    className="relative flex w-full touch-none select-none items-center"
-    {...props}
-  >
-    <SliderPrimitive.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20">
-      <SliderPrimitive.Range className="absolute h-full bg-primary" />
-    </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
-  </SliderPrimitive.Root>
-));
-Slider.displayName = SliderPrimitive.Root.displayName;
-
-export { Slider };
 
 const Studio = ({ dispatch, state }: { dispatch: Function; state: any }) => {
   const [accountHint, setaccountHint]: any[] = useState(null);
---
+++ src/customTypings/empty-types.d.ts
@@ -0,0 +1,4 @@
+declare module "react-gcolor-picker" {
+  const noTypesYet: any;
+  export default noTypesYet;
+}
