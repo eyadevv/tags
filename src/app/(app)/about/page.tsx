34ae9dfd0fe06a1f2f628b7c983f32c3aa565000
eyadevv <eyadevv@gmail.com>
2023-10-10 08:52:38 +0200

    order tags & fix about us content

--- src/app/(app)/about/page.tsx
+++ src/app/(app)/about/page.tsx
@@ -3,8 +3,9 @@ const page = () => {
     <main className="w-11/12 text-center h-full flex justify-center items-center">
       <p className="text-2xl leading-8">
         This Project is Developed By
-        <span className="font-bold"> Eyad Faisal</span> ,and it's part of The
-        fintech Project <span className="font-bold">"waseet"</span>
+        <span className="font-bold"> Eyad Faisal</span> ,and it`&#39;`s part of
+        The fintech Project
+        <span className="font-bold">`&#34;`waseet`&#34;`</span>
       </p>
     </main>
   );
