05b94e0d1ce122b9680095fa1b0229afe5136731
eyadevv <eyadevv@gmail.com>
2023-10-17 20:51:07 +0200

    fix about us

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -19,40 +19,12 @@ async function main() {
 main();
 const data: any = [
   {
-    bank: "ocash",
-    font: "mono",
-    icons: false,
-    barcode: true,
-  },
-  {
-    bank: "ocash",
-    font: "mono",
-    icons: false,
-    barcode: true,
-  },
-  {
-    bank: "fawry",
-    text: "white",
-    font: "mono",
-    icons: false,
-    barcode: true,
-  },
-  {
-    text: "white",
-    font: "mono",
-    icons: false,
-    barcode: true,
-  },
-  {
-    bank: "ocash",
-    font: "mono",
-    icons: false,
-    barcode: true,
-  },
-  {
+    bg: "#f44336",
     bank: "bankak",
-    font: "mono",
-    icons: false,
-    barcode: true,
+    bgStyle: "gradient",
+    bankRadius: "5",
+    tagRadius: "3",
   },
+  // generate 10 more Tags with more complex graidents
+  {},
 ];
--- src/app/(app)/about/page.tsx
+++ src/app/(app)/about/page.tsx
@@ -3,9 +3,9 @@ const page = () => {
     <main className="w-11/12 text-center h-full flex justify-center items-center">
       <p className="text-2xl leading-8">
         This Project is Developed By
-        <span className="font-bold"> Eyad Faisal</span> ,and it`&#39;`s part of
-        The fintech Project
-        <span className="font-bold">`&#34;`waseet`&#34;`</span>
+        <span className="font-bold"> Eyad Faisal</span> ,and is part of The
+        fintech Project
+        <span className="font-bold">waseet</span>
       </p>
     </main>
   );
